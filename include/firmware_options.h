30d44d00f5405aa7b4762ecb108c58a65ed3fdde
MahmoudsEl-Din <m.s.el-din@hotmail.com>
2023-07-03 16:10:19 +0300

    Add files via upload

---
+++ include/firmware_options.h
@@ -0,0 +1,26 @@
+/*
+  Firmware_include.h - header to be included in libs for external configuration via Firmware ifdefs
+
+  Copyright (C) 2021  Theo Arends
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef __Firmware_INCLUDE__
+#define __Firmware_INCLUDE__
+
+#include "../Firmware/my_user_config.h"
+#include "../Firmware/include/Firmware_configurations.h"
+
+#endif // __Firmware_INCLUDE__
